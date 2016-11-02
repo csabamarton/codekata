09d4167171e505c35a664db09e1245cf937cd181
csabamarton <martoncsab@gmail.com>
2016-11-02 12:46:14 +0100

    daily coding kata, binary trees

--- algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/HuffmanDecoding.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/HuffmanDecoding.java
@@ -1,7 +1,6 @@
 package com.csmarton.hackerrank.datastrucure.tree;
 
 public class HuffmanDecoding {
-	private static final char INTERNAL_NODE_DATA = '\0';
 
 	class Node {
 		int freq;
@@ -11,25 +10,28 @@ public class HuffmanDecoding {
 
 	}
 
-	void decode(Node root, String s)
+	private static final char INTERNAL_NODE_DATA = '\0';
+
+	void decode(String s, Node root)
 	{
+
 		boolean hasNew = s.length() > 0;
 
 		Node tempNode = root;
-
 		while (hasNew) {
+
 			char c = s.charAt(0);
 			s = s.substring(1);
-
 			if (tempNode.data == INTERNAL_NODE_DATA) {
 				if (c == '0') {
-					tempNode = root.left;
+					tempNode = tempNode.left;
 				} else {
-					tempNode = root.right;
+					tempNode = tempNode.right;
 				}
+			}
 
-			} else {
-				System.out.print(root.data);
+			if (tempNode.data != INTERNAL_NODE_DATA) {
+				System.out.print(tempNode.data);
 				tempNode = root;
 				hasNew = s.length() > 0;
 			}
--- algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/LowestCommonAncestor.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/LowestCommonAncestor.java
@@ -1,13 +1,16 @@
 package com.csmarton.hackerrank.datastrucure.tree;
 
 public class LowestCommonAncestor {
-	static Node lca(Node root,int v1,int v2)
+	static Node lca(Node root, int v1, int v2)
 	{
+		if (root == null) {
+			return root;
+		}
 		int data = root.data;
-		if((v1 > data && v2 < data) || (v1 < data && v2 > data)) {
+		if ((v1 > data && v2 < data) || (v1 < data && v2 > data) || v1 == data || v2 == data) {
 			return root;
 		} else {
-			if(v1 < data){
+			if (v1 < data) {
 				return lca(root.left, v1, v2);
 			} else {
 				return lca(root.right, v1, v2);
