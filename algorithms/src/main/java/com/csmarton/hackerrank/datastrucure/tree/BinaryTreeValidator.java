b4ab0837f4583e0214e690247072eaaedf5e1ad3
csabamarton <martoncsab@gmail.com>
2016-11-02 12:53:52 +0100

    binary tree validatior

--- algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/BinaryTreeValidator.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/tree/BinaryTreeValidator.java
@@ -1,32 +1,33 @@
 package com.csmarton.hackerrank.datastrucure.tree;
 
-public class BinaryTreeValidator
-{
-	boolean checkBST(Node root) {
-		if(root == null) {
+public class BinaryTreeValidator {
+	boolean checkBST(Node root)
+	{
+		if (root == null) {
 			return true;
 		}
 
 		return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
 
-	boolean checkBST(Node root, Integer minLimit, Integer maxLimit) {
+	boolean checkBST(Node root, Integer minLimit, Integer maxLimit)
+	{
 		boolean isOK = true;
 
-		if(root.left != null) {
-			if(root.left.data > root.data || root.left.data < minLimit){
+		if (root.left != null) {
+			if (root.left.data > root.data || root.left.data < minLimit) {
 				return false;
 			} else {
 				isOK = checkBST(root.left, minLimit, root.data);
 			}
 		}
 
-		if(!isOK){
+		if (!isOK) {
 			return false;
 		}
 
-		if(root.right != null) {
-			if(root.right.data < root.data || root.right.data > maxLimit) {
+		if (root.right != null) {
+			if (root.right.data < root.data || root.right.data > maxLimit) {
 				return false;
 			} else {
 				return checkBST(root.right, root.data, maxLimit);
