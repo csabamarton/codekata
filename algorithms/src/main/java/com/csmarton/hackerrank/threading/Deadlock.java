9947f499a0ed2f099428f4981bda44e82ba1c078
csabamarton <martoncsab@gmail.com>
2016-11-24 10:34:31 +0100

    deadlock solution

--- algorithms/src/main/java/com/csmarton/hackerrank/threading/Deadlock.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/threading/Deadlock.java
@@ -7,9 +7,9 @@ public class Deadlock {
 		Object obj2 = new Object();
 		Object obj3 = new Object();
 
-		Thread t1 = new Thread(new SyncThread(obj1, obj2), "t1");
-		Thread t2 = new Thread(new SyncThread(obj2, obj3), "t2");
-		Thread t3 = new Thread(new SyncThread(obj3, obj1), "t3");
+		Thread t1 = new Thread(new SyncThreadWithDeadLock(obj1, obj2), "t1");
+		Thread t2 = new Thread(new SyncThreadWithDeadLock(obj2, obj3), "t2");
+		Thread t3 = new Thread(new SyncThreadWithDeadLock(obj3, obj1), "t3");
 
 		t1.start();
 		Thread.sleep(5000);
@@ -20,11 +20,11 @@ public class Deadlock {
 
 }
 
-class SyncThread implements Runnable {
+class SyncThreadWithDeadLock implements Runnable {
 	private Object obj1;
 	private Object obj2;
 
-	public SyncThread(Object o1, Object o2)
+	public SyncThreadWithDeadLock(Object o1, Object o2)
 	{
 		this.obj1 = o1;
 		this.obj2 = o2;
@@ -46,6 +46,44 @@ class SyncThread implements Runnable {
 			System.out.println(name + " released lock on " + obj2);
 		}
 		System.out.println(name + " released lock on " + obj1);
+		System.out.println(name + " finished execution.");
+	}
+
+	private void work()
+	{
+		try {
+			Thread.sleep(30000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class SysncThreadWithoutDeadLock implements Runnable {
+	private Object obj1;
+	private Object obj2;
+
+	public SysncThreadWithoutDeadLock(Object obj1, Object obj2)
+	{
+		this.obj1 = obj1;
+		this.obj2 = obj2;
+	}
+
+	public void run() {
+		String name = Thread.currentThread().getName();
+		System.out.println(name + " acquiring lock on " + obj1);
+		synchronized (obj1) {
+			System.out.println(name + " acquired lock on " + obj1);
+			work();
+		}
+		System.out.println(name + " released lock on " + obj1);
+		System.out.println(name + " acquiring lock on " + obj2);
+		synchronized (obj2) {
+			System.out.println(name + " acquired lock on " + obj2);
+			work();
+		}
+		System.out.println(name + " released lock on " + obj2);
+
 		System.out.println(name + " finished execution.");
 	}
 
