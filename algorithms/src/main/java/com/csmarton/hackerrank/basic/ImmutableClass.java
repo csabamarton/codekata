1db61d8fed220f38c3fdebc26ce1343cfc133fa1
csabamarton <martoncsab@gmail.com>
2016-11-24 08:18:46 +0100

    immutable class

---
+++ algorithms/src/main/java/com/csmarton/hackerrank/basic/ImmutableClass.java
@@ -0,0 +1,16 @@
+package com.csmarton.hackerrank.basic;
+
+public final class ImmutableClass
+{
+	public ImmutableClass(int number)
+	{
+		this.number = number;
+	}
+
+	private int number;
+
+	public int getNumber()
+	{
+		return number;
+	}
+}
