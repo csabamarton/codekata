ff220148e924b86a63512ba45d1125e45b871bba
csmarton <martoncsab@gmail.com>
2021-11-24 08:25:06 +0100

    refactor: extract to separated methods

--- interviews/src/main/java/LookForWords.java
+++ interviews/src/main/java/LookForWords.java
@@ -1,77 +1,88 @@
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LookForWords {
 
     public static void main(String[] args) throws IOException {
-
         LookForWords lookForWords = new LookForWords();
+        lookForWords.processInputFile("example");
+    }
+
+    private void processInputFile(String inputFileName) throws IOException {
         ClassLoader classLoader = LookForWords.class.getClassLoader();
-        String inputFilename = "example";
-        InputStream inputStream = classLoader.getResourceAsStream(inputFilename + ".txt");
+        InputStream inputStream = classLoader.getResourceAsStream(inputFileName + ".txt");
 
+        Map<String, String> result = readFromInputStream(inputStream);
 
-        String expected_value = "Hello, world!";
+        writeOutResult(result, inputFileName);
 
-        Path source = Paths.get(LookForWords.class.getResource("/").getPath());
-        Path path = Paths.get("src/test/resources/fileTest.txt");
-
-        BufferedReader reader = Files.newBufferedReader(path);
-        String line = reader.readLine();
-
-        try {
-            Map<String, String> result = lookForWords.readFromInputStream(inputStream);
-
-
-            Writer writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(new File(source.toAbsolutePath() + inputFilename + "-index.txt")), StandardCharsets.UTF_8));
-            result.forEach((key, value) -> {
-                try {
-                    writer.write(key + " " + value + System.lineSeparator());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            writer.flush();
-            writer.close();
-
-            System.out.println(result);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(result);
     }
 
     private Map readFromInputStream(InputStream inputStream)
             throws IOException {
-        StringBuilder resultStringBuilder = new StringBuilder();
-
         Map<String, String> wordsInLine = new HashMap<>();
-
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             int lineNum = 1;
 
             while ((line = br.readLine()) != null) {
-                String[] words = line.split(" ");
-                for (int i = 0; i < words.length; i++) {
-                    String word = words[i];
-                    if(!wordsInLine.containsKey(word)) {
-                        wordsInLine.put(word, Integer.toString(lineNum));
-                    } else {
-                        String wordLineNums = wordsInLine.get(word);
-                        wordsInLine.put(word, wordLineNums + "," + lineNum);
-                    }
-                }
-                //resultStringBuilder.append(line).append("\n");
+                processLine(wordsInLine, line, lineNum, true);
                 lineNum++;
             }
         }
         return wordsInLine;
+    }
+
+    private void processLine(Map<String, String> wordsInLine, String line, int lineNum, boolean caseSensititve) {
+        Stream<String> stream = Arrays.stream(line.split(" "));
+
+        if(caseSensititve) {
+            stream = stream.map(String::toLowerCase);
+        }
+        List<String> words = stream.distinct().collect(Collectors.toList());
+
+        for (String word : words) {
+            if (!wordsInLine.containsKey(word)) {
+                wordsInLine.put(word, Integer.toString(lineNum));
+            } else {
+                String wordLineNums = wordsInLine.get(word);
+                wordsInLine.put(word, wordLineNums + "," + lineNum);
+            }
+        }
+    }
+
+    private void writeOutResult(Map<String, String> result, String inputFileName) throws IOException {
+        SortedSet<String> keys = new TreeSet<>(result.keySet());
+
+        Path fullResultFilePath = setupResultFile(inputFileName);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(fullResultFilePath))
+        {
+            for (String key: keys) {
+                writer.write(key + " " + result.get(key) + System.lineSeparator());
+            }
+        }
+    }
+
+    private Path setupResultFile(String inputFileName) throws IOException {
+        Path targetPath = Paths.get(this.getClass().getResource("/").getPath());
+        Path resultFolder = Paths.get(targetPath.toAbsolutePath() + "/result/");
+
+        //cleanUp
+        FileUtils.deleteDirectory(resultFolder.toFile());
+        Files.createDirectories(resultFolder);
+
+        Path fullResultFilePath = resultFolder.resolve(inputFileName + "-index.txt");
+        return fullResultFilePath;
     }
 }
