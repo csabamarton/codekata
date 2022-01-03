591c56eb2aafd1a4e8d5699c0899015f5d82267b
Marton Csaba <martoncsab@gmail.com>
2022-01-03 08:49:58 +0100

    Solved MatchingSocks problem

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/MatchingSocks.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/MatchingSocks.java
@@ -1,7 +1,27 @@
 package com.csmarton.hackerrank.algorithms.warmup;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MatchingSocks {
     public int solution(int[] paramArray) {
-        return 0;
+        Map<Integer, Integer> colorMap = new HashMap<>();
+
+        Arrays.stream(paramArray).boxed().forEach(color -> {
+            if(colorMap.containsKey(color)) {
+                colorMap.put(color, colorMap.get(color) + 1);
+            } else {
+                colorMap.put(color, 1);
+            }
+        });
+
+        int numOfPairedSocks = 0;
+
+        for (Integer value : colorMap.values()) {
+            numOfPairedSocks = numOfPairedSocks + value / 2;
+        }
+
+        return numOfPairedSocks;
     }
 }
