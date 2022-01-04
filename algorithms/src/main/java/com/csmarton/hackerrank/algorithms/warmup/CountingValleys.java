3553328bd51ccbdffbe495a179cd6505cceb7bca
Marton Csaba <martoncsab@gmail.com>
2022-01-04 08:26:38 +0100

    Morning kata, solved hackerrank

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/CountingValleys.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/CountingValleys.java
@@ -1,7 +1,28 @@
 package com.csmarton.hackerrank.algorithms.warmup;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CountingValleys {
     public int solve(int steps, String path) {
-        return -1;
+        boolean isItAValley = false;
+        int currentAltitude = 0;
+        int numOfValleys = 0;
+
+        List<Character> stepList = path.chars().mapToObj(i -> (char) i)
+                .collect(Collectors.toList());
+
+        for (Character step: stepList) {
+            currentAltitude = currentAltitude + (step.charValue() == 'U' ? 1 : -1);
+
+            if (isItAValley && currentAltitude == 0) {
+                numOfValleys++;
+                isItAValley = false;
+            } else if (!isItAValley && currentAltitude < 0) {
+                isItAValley = true;
+            }
+        }
+
+        return numOfValleys;
     }
 }
