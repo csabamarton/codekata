2704a17bdafe12e1d53e252285a4ec44e477f153
Marton Csaba <martoncsab@gmail.com>
2022-01-05 22:52:11 +0100

    Morning kata: Solved RepeatedString problem second solution

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/RepeatedString.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/warmup/RepeatedString.java
@@ -1,7 +1,51 @@
 package com.csmarton.hackerrank.algorithms.warmup;
 
 public class RepeatedString {
-    public long solution(String text) {
-        return 0;
+    public int solution(String s, long n) {
+        long index = 0;
+        int letterIndex = 0;
+        int numOfOccurences = 0;
+
+        char[] letters = s.toCharArray();
+
+        while (index < n) {
+            char letter = letters[letterIndex];
+            if (letter == 'a')
+                numOfOccurences++;
+
+            index++;
+            letterIndex++;
+            if (letterIndex == letters.length)
+                letterIndex = 0;
+        }
+
+        return numOfOccurences;
+    }
+
+    public long solution2(String s, long n) {
+        long numOfRepetitions = n / s.length();
+        int letterIndex = 0;
+        long numOfOccurences = 0;
+        char[] letters = s.toCharArray();
+
+        while (letterIndex < letters.length) {
+            char letter = letters[letterIndex];
+            if (letter == 'a')
+                numOfOccurences++;
+
+            letterIndex++;
+        }
+
+        numOfOccurences = numOfOccurences * numOfRepetitions;
+
+        long modulo = n - (letters.length * numOfRepetitions);
+
+        for (int i = 0; i < modulo; i++) {
+            char letter = letters[i];
+            if (letter == 'a')
+                numOfOccurences++;
+        }
+
+        return numOfOccurences;
     }
 }
--- algorithms/src/test/java/com/csmarton/hackerrank/algorithms/warmup/RepeatedStringTest.java
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/warmup/RepeatedStringTest.java
@@ -1,6 +1,8 @@
 package com.csmarton.hackerrank.algorithms.warmup;
 
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
@@ -11,12 +13,23 @@ class RepeatedStringTest {
 
     public static Stream<Arguments> paramProvider() {
         return Stream.of(
-                Arguments.of("", 2)
+                Arguments.of("aba", 10, 7),
+                Arguments.of("abcac", 10, 4)
         );
     }
 
-    public void testSolution(String text, long expectedNumOfOccurence) {
-        long result = repeatedString.solution(text);
+    @ParameterizedTest
+    @MethodSource("paramProvider")
+    public void testSolution(String text, long textLength, int expectedNumOfOccurence) {
+        int result = repeatedString.solution(text, textLength);
+
+        assertEquals(expectedNumOfOccurence, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramProvider")
+    public void testSolution2(String text, long textLength, long expectedNumOfOccurence) {
+        long result = repeatedString.solution2(text, textLength);
 
         assertEquals(expectedNumOfOccurence, result);
     }
