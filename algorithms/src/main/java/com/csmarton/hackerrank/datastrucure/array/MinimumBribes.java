6480ab28099d4705a28e85cef095dcd457f06574
Marton Csaba <martoncsab@gmail.com>
2022-01-07 23:32:44 +0100

    Morning Kata: Solved test case for Minimum Bribes problem

--- algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/array/MinimumBribes.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/array/MinimumBribes.java
@@ -1,6 +1,9 @@
 package com.csmarton.hackerrank.datastrucure.array;
 
 import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class MinimumBribes {
 
@@ -27,18 +30,23 @@ public class MinimumBribes {
     */
 
     public int solution(List<Integer> queue) throws Exception {
+        Map<Integer, Integer> queuePositions = IntStream.range(0, queue.size())
+                .boxed()
+                .collect(toMap(queue::get, i -> i));
+
         int numOfBribes = 0;
-
         int fixQueueIndex = 0;
-
         Map<Integer, Integer> bribeMap = new HashMap<>();
 
         while (fixQueueIndex < queue.size()) {
-            int nextIndex = findNextSortingElement(fixQueueIndex, queue);
+            int nextIndex = queuePositions.get(fixQueueIndex + 1);
 
             boolean isAtRightPosition = nextIndex == fixQueueIndex;
 
             while(!isAtRightPosition) {
+                queuePositions.put(queue.get(nextIndex), nextIndex - 1);
+                queuePositions.put(queue.get(nextIndex - 1), nextIndex );
+
                 Collections.swap(queue, nextIndex - 1, nextIndex);
 
                 Integer briber = queue.get(nextIndex);
@@ -63,46 +71,5 @@ public class MinimumBribes {
         }
 
         return numOfBribes;
-    }
-
-    private int findNextSortingElement(int fixQueueIndex, List<Integer> queue) throws Exception {
-        for (int i = 0; i < queue.size(); i++) {
-            if (queue.get(i) == fixQueueIndex + 1)
-                return i;
-        }
-
-        throw new Exception("Wrong test Case. Missing number: " + fixQueueIndex + 1);
-    }
-
-    public int solution2(List<Integer> initialQueue) {
-        int sumDistances = 0;
-
-        boolean notPerfectQueue = true;
-
-        //while (notPerfectQueue) {
-            notPerfectQueue = false;
-            for (int i = 0; i < initialQueue.size(); i++) {
-                if (initialQueue.get(i) - i - 1 != 0) {
-                    notPerfectQueue = true;
-                    break;
-                }
-            }
-
-            if(false){
-                for (int i = 0; i < initialQueue.size(); i++) {
-
-                    int currentDist = initialQueue.get(i) - i - 1;
-                    int nextDist = initialQueue.get(i + 1) - i;
-
-                    if(currentDist > 0 && nextDist < 0) {
-                        Integer temp = initialQueue.get(i);
-                        initialQueue.set(i, initialQueue.get(i+1));
-                        initialQueue.set(i + 1, temp);
-                    }
-                }
-            }
-        //}
-
-        return sumDistances;
     }
 }
