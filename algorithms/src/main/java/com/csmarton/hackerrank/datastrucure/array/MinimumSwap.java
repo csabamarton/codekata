b54a8819b01fbe4a6cc8960a133ed07adfedd8f2
Marton Csaba <martoncsab@gmail.com>
2022-01-08 15:53:38 +0100

    Morning Kata: Solved test case for Minimum Swap problem

--- algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/array/MinimumSwap.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/array/MinimumSwap.java
@@ -1,7 +1,44 @@
 package com.csmarton.hackerrank.datastrucure.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumSwap {
+    /**
+     * Given an array with n element find the and swap the elements to the right position.
+     * Starting from position 0 until reach n-1
+     *
+     * @param arr
+     * @return number of minumum swap for making order (asc) a given unordered array of Integer
+     */
     public int solution(int[] arr) {
-        return -1;
+        int numOfSwap = 0;
+
+        int currentPosition = 0;
+
+        Map<Integer, Integer> numbersWithIndex = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            numbersWithIndex.put(arr[i], i);
+        }
+
+        while (currentPosition < arr.length) {
+            int nextIndex = numbersWithIndex.get(currentPosition + 1);
+
+            if(nextIndex != currentPosition) {
+                //swapping array
+                int temp = arr[nextIndex];
+                arr[nextIndex] = arr[currentPosition];
+                arr[currentPosition] = temp;
+                //swapping map
+                numbersWithIndex.put(temp, currentPosition);
+                numbersWithIndex.put(arr[nextIndex], nextIndex);
+
+                numOfSwap++;
+            }
+
+            currentPosition++;
+        }
+
+        return numOfSwap;
     }
 }
