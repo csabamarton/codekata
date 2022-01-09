2d955eeed71bca1f494926c4c91d6b366135a107
Marton Csaba <martoncsab@gmail.com>
2022-01-09 15:53:47 +0100

    Morning Kata: Initail failed test case for Check Magazine problem

---
+++ algorithms/src/main/java/com/csmarton/hackerrank/datastrucure/dictionarieshashmaps/CheckMagazine.java
@@ -0,0 +1,9 @@
+package com.csmarton.hackerrank.datastrucure.dictionarieshashmaps;
+
+import java.util.List;
+
+public class CheckMagazine {
+    public boolean solution(List<String> magazine, List<String> note) {
+        return false;
+    }
+}
---
+++ algorithms/src/test/java/com/csmarton/hackerrank/datastrucure/dictionarieshashmaps/CheckMagazineTest.java
@@ -0,0 +1,30 @@
+package com.csmarton.hackerrank.datastrucure.dictionarieshashmaps;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckMagazineTest {
+    CheckMagazine checkMagazine = new CheckMagazine();
+
+    private static Stream<Arguments> paramProvider() {
+        return Stream.of(
+                Arguments.of(List.of("give", "me", "one", "grand", "today", "night"),
+                        List.of("give", "one", "grand", "today"), true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramProvider")
+    public void testSolution(List<String> magazine, List<String> note, boolean expectedResult) {
+        boolean result = checkMagazine.solution(magazine, note);
+
+        assertEquals(expectedResult, result);
+
+    }
+}
