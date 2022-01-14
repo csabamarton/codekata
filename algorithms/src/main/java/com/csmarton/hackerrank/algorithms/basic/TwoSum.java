27a1bbc889ca7e73ea63a40c54fc0f3d8289d5e6
Marton Csaba <martoncsab@gmail.com>
2022-01-14 20:21:28 +0100

    Morning Kata: Initail failed test case for TwoSum problem

---
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/basic/TwoSum.java
@@ -0,0 +1,7 @@
+package com.csmarton.hackerrank.algorithms.basic;
+
+public class TwoSum {
+    public int[] solution(int[] numbers, int target) {
+        return new int[]{2,3};
+    }
+}
---
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/basic/TwoSumTest.java
@@ -0,0 +1,28 @@
+package com.csmarton.hackerrank.algorithms.basic;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+    TwoSum twoSum = new TwoSum();
+
+    private static Stream<Arguments> paramProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2,7,11,15}, 9, new int[]{0,1})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramProvider")
+    public void testSolution(int[] numbers, int target, int[] expectedPair) {
+        int[] result = twoSum.solution(numbers, target);
+
+        assertArrayEquals(expectedPair, result);
+    }
+
+}
