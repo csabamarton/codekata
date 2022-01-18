357cfb6dc7184829c2f3b6e1d65a10d50867ce3e
Marton Csaba <martoncsab@gmail.com>
2022-01-18 17:59:39 +0100

    Test case resources and another passed test case

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/basic/CountTriplets.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/basic/CountTriplets.java
@@ -1,9 +1,109 @@
 package com.csmarton.hackerrank.algorithms.basic;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class CountTriplets {
+
+    public long solution3(List<Long> numbers, long ratio) {
+        long numOfTriplets = 0;
+
+
+        if(ratio == 1) {
+            Map<Long, Integer> numOfChildren = new HashMap<>();
+            for (int i = 0; i < numbers.size(); i++) {
+                Long baseNumber = ((Number) numbers.get(i)).longValue();
+                if (numOfChildren.containsKey(baseNumber)) {
+                    numOfChildren.put(baseNumber, numOfChildren.get(baseNumber) + 1);
+                } else
+                    numOfChildren.put(baseNumber, 1);
+            }
+            for (Map.Entry<Long, Integer> entry : numOfChildren.entrySet()) {
+                int size = entry.getValue();
+                if (size < 3)
+                    continue;
+                if(size < 4) {
+                    numOfTriplets++;
+                    continue;
+                }
+                long mainIncr = 4;
+                long middleIncr = 3;
+
+                int base = 2;
+                for (int i = 0; i < size - 4; i++) {
+                    base++;
+                    middleIncr += base;
+                    mainIncr = mainIncr + middleIncr;
+                }
+                numOfTriplets += mainIncr;
+            }
+
+            return numOfTriplets;
+        }
+
+        Map<Long, List<Integer>> numOfOccurences = new HashMap<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            Long baseNumber = ((Number) numbers.get(i)).longValue();
+            if(numOfOccurences.containsKey(baseNumber)) {
+                numOfOccurences.get(baseNumber).add(i);
+            } else
+                numOfOccurences.put(baseNumber, new ArrayList<>(Arrays.asList(i)));
+        }
+        Map<Long, Integer> leftSkip = new HashMap<>();
+        Map<Long, Integer> rightSkip = new HashMap<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            Long baseNumber = ((Number) numbers.get(i)).longValue();
+
+            if(rightSkip.containsKey(baseNumber) && rightSkip.get(baseNumber) < i)
+                continue;
+            if(leftSkip.containsKey(baseNumber) && leftSkip.get(baseNumber) > i)
+                continue;
+
+            final int index = i;
+
+            long rightCount = 0;
+            long leftCount = 0;
+            long multiple = baseNumber * ratio;
+            if (numOfOccurences.containsKey(multiple))
+            {
+                rightCount = numOfOccurences.get(multiple).stream().parallel().filter(num -> num > index).count();
+            }
+
+            if(baseNumber % ratio == 0) {
+                long div = baseNumber / ratio;
+                if(div>0) {
+                    if (numOfOccurences.containsKey(div)) {
+                        leftCount = numOfOccurences.get(div).stream().parallel().filter(num -> num < index).count();
+                    }
+                }
+            }
+
+            if (rightCount != 0 && leftCount != 0) {
+                numOfTriplets += rightCount * leftCount;
+            } else {
+                if(rightCount == 0){
+                    if(rightSkip.containsKey(baseNumber)) {
+                        if(rightSkip.get(baseNumber) > i)
+                            rightSkip.put(baseNumber, i);
+                    } else
+                        rightSkip.put(baseNumber, i);
+                }
+                if(leftCount == 0){
+                    if(leftSkip.containsKey(baseNumber)) {
+                        if(leftSkip.get(baseNumber) < i)
+                            leftSkip.put(baseNumber, i);
+                    } else
+                        leftSkip.put(baseNumber, i);
+                }
+            }
+        }
+
+        return numOfTriplets;
+    }
+
 
     public long solution2(List<Long> numbers, long ratio) {
         long numOfTriplets = 0;
@@ -43,6 +143,7 @@ public class CountTriplets {
 
         Map<Long, List<Integer>> numOfOccurences = new HashMap<>();
         Map<Integer, Integer> numOfChild = new HashMap<>();
+
         for (int i = 0; i < numbers.size(); i++) {
             Long baseNumber = ((Number) numbers.get(i)).longValue();
             if(numOfOccurences.containsKey(baseNumber)) {
@@ -50,9 +151,6 @@ public class CountTriplets {
             } else
                 numOfOccurences.put(baseNumber, new ArrayList<>(Arrays.asList(i)));
         }
-
-
-
 
         Map<Integer, List<Integer>> numMap= new HashMap<>();
 
@@ -62,24 +160,76 @@ public class CountTriplets {
             final int index = i;
 
             List<Integer> ratioedIndexes = new ArrayList<>();
+
             if (numOfOccurences.containsKey(baseNumber * ratio))
             {
                 ratioedIndexes = numOfOccurences.get(baseNumber * ratio);
-                ratioedIndexes = ratioedIndexes.stream().filter(num -> num > index).collect(Collectors.toList());
+                ratioedIndexes = ratioedIndexes.stream().filter(num -> num > index).collect(toList());
             }
 
-            numMap.put(i, ratioedIndexes);
+            if(!ratioedIndexes.isEmpty())
+                numMap.put(i, ratioedIndexes);
+
             numOfChild.put(i, ratioedIndexes.size());
         }
 
-        for (int i = 0; i < numbers.size(); i++) {
-            List<Integer> secondNode = numMap.get(i);
+       for(Map.Entry<Integer, List<Integer>> entry : numMap.entrySet()){
+            List<Integer> secondNode = entry.getValue();
 
-            long count = secondNode.stream().parallel().map(key -> numOfChild.get(key)).mapToInt(Integer::valueOf).sum();
+            long count = secondNode.stream().parallel().filter(key -> numOfChild.get(key) > 0).map(key -> numOfChild.get(key)).mapToInt(Integer::valueOf).sum();
 
             numOfTriplets += count;
         }
 
         return numOfTriplets;
+    }
+
+
+    static long countTriplets(List<Long> arr, long r) {
+
+        Map<Long, Long> rightMap = getOccurenceMap(arr);
+        Map<Long, Long> leftMap = new HashMap<>();
+        long numberOfGeometricPairs = 0;
+
+        for (Number val : arr) {
+            long countLeft = 0;
+            long countRight = 0;
+            long lhs = 0;
+            long longVal = val.longValue();
+            long rhs = longVal * r;
+            if (longVal % r == 0) {
+                lhs = longVal / r;
+            }
+            Long occurence = rightMap.get(val);
+            if (occurence != null)
+                rightMap.put(longVal, occurence - 1L);
+
+            if (rightMap.containsKey(rhs)) {
+                countRight = rightMap.get(rhs);
+            }
+            if (leftMap.containsKey(lhs)) {
+                countLeft = leftMap.get(lhs);
+            }
+            numberOfGeometricPairs += countLeft * countRight;
+            insertIntoMap(leftMap, longVal);
+        }
+        return numberOfGeometricPairs;
+    }
+
+    private static Map<Long, Long> getOccurenceMap(List<Long> test) {
+        Map<Long, Long> occurenceMap = new HashMap<>();
+        for (Number val : test) {
+            insertIntoMap(occurenceMap, val.longValue());
+        }
+        return occurenceMap;
+    }
+
+    private static void insertIntoMap(Map<Long, Long> occurenceMap, Long val) {
+        if (!occurenceMap.containsKey(val)) {
+            occurenceMap.put(val, 1L);
+        } else {
+            Long occurence = occurenceMap.get(val);
+            occurenceMap.put(val, occurence + 1L);
+        }
     }
 }
--- algorithms/src/test/java/com/csmarton/hackerrank/algorithms/basic/CountTripletsTest.java
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/basic/CountTripletsTest.java
@@ -1,32 +1,66 @@
 package com.csmarton.hackerrank.algorithms.basic;
 
+import org.junit.Before;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountTripletsTest {
     CountTriplets countTriplets = new CountTriplets();
 
-    private static Stream<Arguments> paramProvider() {
+    private Path workingDir;
+
+    @Before
+    public void init() {
+        this.workingDir = Path.of("", "src/test/resources");
+    }
+
+    private static Stream<Arguments> paramProvider() throws IOException {
         List<Long> tests = new ArrayList<>();
         for (int i = 0; i < 100000; i++) {
             tests.add(1237L);
         }
 
+        Path workingDir = Path.of("", "src/test/resources");
+        Path file = workingDir.resolve("triplet2");
+        InputStream inputStream = CountTripletsTest.class.getResourceAsStream("src/test/resources/triplet");
+
+        BufferedReader reader = new BufferedReader(new FileReader(file.toString()));
+
+        String content = Files.readString(file);
+        BufferedReader bufferedReader = reader;
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        bufferedReader.close();
+
         return Stream.of(
-                Arguments.of(List.of(1, 1, 1, 1, 1,1, 2, 2, 2, 2, 2, 2), 1, 40)
+                //Arguments.of(arr, r, 2325652489L),
+               // Arguments.of(arr, r, 1339347780085L),
+                //Arguments.of(List.of(1, 1, 1, 1, 1,1, 2, 2, 2, 2, 2, 2), 1, 40)
                // Arguments.of(tests, 1, 166661666700000L)
-               // Arguments.of(List.of(1, 5, 1, 5, 25, 125), 5, 5),
-                //Arguments.of(List.of(1,4, 16, 64), 4, 2),
-                //Arguments.of(List.of(1,2,2,4), 2, 2),
-                //Arguments.of(List.of(1, 3, 9, 9, 27, 81), 3, 6),
+                Arguments.of(List.of(6,9,1, 5, 1, 5, 25, 125), 5, 5),
+                Arguments.of(List.of(1,4, 16, 64), 4, 2),
+                Arguments.of(List.of(1,2,2,4), 2, 2),
+                Arguments.of(List.of(1, 3, 9, 9, 27, 81), 3, 6)
                 //Arguments.of(List.of(1, 1, 1), 1, 4),
                 //Arguments.of(List.of(1, 1, 1, 1), 1, 4),
                 //Arguments.of(List.of(1, 1, 1, 1, 1), 1, 10),
@@ -43,7 +77,7 @@ class CountTripletsTest {
     @ParameterizedTest
     @MethodSource("paramProvider")
     public void testSolution(List<Long> numbers, long ratio, long expectedNumOfTriplets) {
-        long result = countTriplets.solution2(numbers, ratio);
+        long result = countTriplets.countTriplets(numbers, ratio);
 
         assertEquals(expectedNumOfTriplets, result);
     }
