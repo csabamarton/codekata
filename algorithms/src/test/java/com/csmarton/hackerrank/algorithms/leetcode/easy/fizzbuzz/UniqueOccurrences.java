f6cce29002be2ea233766b4991e1288f08aed6c7
csabamarton <martoncsab@gmail.com>
2022-12-13 18:41:47 +0100

    UniqueOccurrences (Leetcode)

---
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/leetcode/easy/fizzbuzz/UniqueOccurrences.java
@@ -0,0 +1,20 @@
+package com.csmarton.hackerrank.algorithms.leetcode.easy.fizzbuzz;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueOccurrences {
+    public boolean solution(int[] arr) {
+        Map<Integer, Integer> occurMap = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            occurMap.put(arr[i] , occurMap.containsKey(arr[i]) ? occurMap.get(arr[i]) + 1 : 1);
+        }
+
+        Set<Integer> distictOccurences = new HashSet<>(occurMap.values());
+
+        return occurMap.values().size() == distictOccurences.size();
+    }
+}
