7750fa16d976b244852bb44e40cd4bbd151f2f56
csabamarton <martoncsab@gmail.com>
2022-12-20 12:50:48 +0100

    One Liner Array sort (Leetcode)

--- algorithms/src/test/java/com/csmarton/hackerrank/algorithms/leetcode/easy/maximumunits/MaxUnits.java
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/leetcode/easy/maximumunits/MaxUnits.java
@@ -5,14 +5,7 @@ import java.util.Comparator;
 
 public class MaxUnits {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        Arrays.sort(boxTypes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1])
-                    return 0;
-                return o1[1] < o2[1] ? 1 : -1;
-            }
-        });
+        sortBoxesWithOneLiner(boxTypes);
 
         int maxUnits = 0;
 
@@ -28,5 +21,20 @@ public class MaxUnits {
         }
 
         return maxUnits;
+    }
+
+    private void sortBoxes(int[][] boxTypes) {
+        Arrays.sort(boxTypes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1])
+                    return 0;
+                return o1[1] < o2[1] ? 1 : -1;
+            }
+        });
+    }
+
+    private void sortBoxesWithOneLiner(int[][] boxTypes) {
+        Arrays.sort(boxTypes, (a, b)-> b[1] - a[1]);
     }
 }
--- algorithms/src/test/java/com/csmarton/hackerrank/algorithms/leetcode/easy/maximumunits/MaxUnitsTest.java
+++ algorithms/src/test/java/com/csmarton/hackerrank/algorithms/leetcode/easy/maximumunits/MaxUnitsTest.java
@@ -14,7 +14,7 @@ class MaxUnitsTest {
 
     static Stream<Arguments> paramProvider() {
         return Stream.of(
-                //Arguments.of(new int[][]{{1,3}, {2,2}, {3,1}}, 4, 8)
+                Arguments.of(new int[][]{{1,3}, {2,2}, {3,1}}, 4, 8),
                 Arguments.of(new int[][]{{5, 10}, {2, 5}, {4, 7}, {3, 9}}, 10, 91)
         );
     }
