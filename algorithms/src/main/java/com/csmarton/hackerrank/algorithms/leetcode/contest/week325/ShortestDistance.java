977b1d1de75ebdbd75aeedb309f47c5f694d5e18
csabamarton <martoncsab@gmail.com>
2022-12-25 13:58:00 +0100

    Subsequence String (Recursion) (easy) (Leetcode)

---
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/contest/week325/ShortestDistance.java
@@ -0,0 +1,33 @@
+package com.csmarton.hackerrank.algorithms.leetcode.contest.week325;
+
+import java.util.Map;
+
+public class ShortestDistance {
+    public int closetTarget(String[] words, String target, int startIndex) {
+        boolean found = false;
+
+        if(words[startIndex].equals(target))
+            return 0;
+
+        int right = startIndex, left = startIndex;
+
+        for (int i = 1; i <= words.length; i++) {
+            right = right == words.length - 1 ? 0 : right + 1;
+            left = left == 0 ? words.length - 1 : left - 1;
+
+            if(words[right].equals(target) || words[left].equals(target))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ShortestDistance shortestDistance = new ShortestDistance();
+
+        System.out.println(shortestDistance.closetTarget(new String[]{"pgmiltbptl","jnkxwutznb","bmeirwjars","ugzyaufzzp","pgmiltbptl","sfhtxkmzwn","pgmiltbptl","pgmiltbptl","onvmgvjhxa","jyzdtwbwqp"}, "pgmiltbptl", 4));
+
+        System.out.println(shortestDistance.closetTarget(new String[]{"hello","i","am","leetcode","hello"}, "hello", 1));
+        System.out.println(shortestDistance.closetTarget(new String[]{"hello","i","am","leetcode","hello"}, "d", 4));
+    }
+}
