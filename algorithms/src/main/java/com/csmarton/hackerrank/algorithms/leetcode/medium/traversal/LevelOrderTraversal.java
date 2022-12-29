6a81da5c3553d520cab51d3521d3681adc48c9c7
csabamarton <martoncsab@gmail.com>
2022-12-29 11:58:31 +0100

    Level Order Binary Tree traversal (medium) (Leetcode)

---
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/medium/traversal/LevelOrderTraversal.java
@@ -0,0 +1,37 @@
+package com.csmarton.hackerrank.algorithms.leetcode.medium.traversal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null)
+            return List.of();
+
+        Map<Integer, List<Integer>> nodes = new HashMap<>();
+
+        traverse(root, nodes, 0);
+
+        return nodes.values().stream().collect(Collectors.toList());
+    }
+
+    private void traverse(TreeNode top, Map<Integer, List<Integer>> nodes, int level) {
+        if(top.left != null) {
+            traverse(top.left, nodes, level + 1);
+        }
+        if(top.right != null) {
+            traverse(top.right, nodes, level + 1);
+        }
+
+        if(nodes.containsKey(level)) {
+            nodes.get(level).add(top.val);
+        } else {
+            List<Integer> levelNodes = new ArrayList<>();
+            levelNodes.add(top.val);
+            nodes.put(level, levelNodes);
+        }
+    }
+}
---
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/medium/traversal/TreeNode.java
@@ -0,0 +1,21 @@
+package com.csmarton.hackerrank.algorithms.leetcode.medium.traversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
