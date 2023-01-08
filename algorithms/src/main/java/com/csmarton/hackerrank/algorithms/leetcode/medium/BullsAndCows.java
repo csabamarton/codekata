2f5bcf55d77546cdd1a014b923985922b95ccbfc
csabamarton <martoncsab@gmail.com>
2023-01-08 10:37:40 +0100

    Bulls and Cows (medium) (Leetcode) Approach 2

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/medium/BullsAndCows.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/medium/BullsAndCows.java
@@ -5,6 +5,41 @@ import java.util.Map;
 
 public class BullsAndCows {
     public String getHint(String secret, String guess) {
+        Map<Character, Integer> digitsFrequency = new HashMap<>();
+        int numOfBulls = 0;
+        int numOfCows = 0;
+
+
+        for (int i = 0; i < secret.length(); i++) {
+            char c = secret.charAt(i);
+
+            if(digitsFrequency.containsKey(c))
+                digitsFrequency.put(c, digitsFrequency.get(c) + 1);
+            else
+                digitsFrequency.put(c, 1);
+        }
+
+        for (int i = 0; i < guess.length(); i++) {
+            char c = guess.charAt(i);
+
+            if(digitsFrequency.containsKey(c)) {
+
+                if(secret.charAt(i) == c) {
+                    numOfBulls++;
+                } else
+                    numOfCows++;
+
+                if (digitsFrequency.get(c) <= 0)
+                    numOfCows--;
+                else
+                    digitsFrequency.put(c, digitsFrequency.get(c) - 1);
+            }
+        }
+
+        return numOfBulls + "A" + numOfCows + "B";
+    }
+
+    public String getHint2(String secret, String guess) {
         StringBuilder sbSecret = new StringBuilder(secret);
 
         int numOfBulls = 0;
