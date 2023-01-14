35595bead978df49d19cce77e8f7cde896476be5
csabamarton <martoncsab@gmail.com>
2023-01-14 16:00:30 +0100

    Remove Nth Node from End (medium) Leetcode Study Plan 75 Level 2 - Fix

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/studyplan/level2/RemoveNthNode.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/studyplan/level2/RemoveNthNode.java
@@ -2,10 +2,39 @@ package com.csmarton.hackerrank.algorithms.leetcode.studyplan.level2;
 
 import com.csmarton.hackerrank.algorithms.leetcode.addtwonumbers.ListNode;
 
+import java.util.List;
+
 public class RemoveNthNode {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-        return head;
+        head = reverseListWithSkip(head, null);
+
+        return reverseListWithSkip(head, n);
     }
+
+    private ListNode reverseListWithSkip(ListNode head, Integer skip) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        int counter = 1;
+
+        while (curr != null) {
+            if(skip != null && counter == skip) {
+                curr = curr.next;
+                continue;
+            }
+
+            ListNode temp = curr.next;
+            curr.next = prev;
+
+
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
 }
+
