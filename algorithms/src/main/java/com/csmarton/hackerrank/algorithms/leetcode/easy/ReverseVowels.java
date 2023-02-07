60c6221280b01c0d936e5a3622bdca90c03d9c09
csabamarton <martoncsab@gmail.com>
2023-02-07 08:30:13 +0100

    Revers Vowels leetcode (easy) - Fix

--- algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/easy/ReverseVowels.java
+++ algorithms/src/main/java/com/csmarton/hackerrank/algorithms/leetcode/easy/ReverseVowels.java
@@ -5,27 +5,25 @@ import java.util.List;
 
 public class ReverseVowels {
     public String reverseVowels(String s) {
-        List<Character> vowels = List.of('a','e','i','o','u');
-
-
+        List<Character> vowels = List.of('a','e','i','o','u', 'A', 'E', 'I', 'O', 'U');
         List<Integer> vowIndexes = new ArrayList<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (vowels.contains(i))
+            if (vowels.contains(s.charAt(i)))
                 vowIndexes.add(i);
         }
 
         int left = 0;
-        int right = vowels.size() - 1;
+        int right = vowIndexes.size() - 1;
 
         StringBuilder sb = new StringBuilder(s);
 
         while (left < right) {
-            char temp = s.charAt(left);
-            char r = s.charAt(right);
+            char temp = s.charAt(vowIndexes.get(left));
+            char r = s.charAt(vowIndexes.get(right));
 
-            sb.setCharAt(left, r);
-            sb.setCharAt(right, temp);
+            sb.setCharAt(vowIndexes.get(left), r);
+            sb.setCharAt(vowIndexes.get(right), temp);
 
             left++;
             right--;
